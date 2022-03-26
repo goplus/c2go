06f3e6e90c78cbb447cd2e5be89aefe092b720f7
xushiwei <x@goplus.org>
2022-03-26 11:33:06 +0800

    github.com/goplus/c2go/cl

---
+++ cl/compile.go
@@ -0,0 +1,17 @@
+package cl
+
+import (
+	"github.com/goplus/c2go/clang/ast"
+	"github.com/goplus/gox"
+)
+
+// -----------------------------------------------------------------------------
+
+type Config struct {
+}
+
+func NewPackage(pkgPath string, file *ast.Node, conf *Config) (p *gox.Package, err error) {
+	return
+}
+
+// -----------------------------------------------------------------------------
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/goplus/c2go
 
 go 1.16
 
-require github.com/json-iterator/go v1.1.12
+require (
+	github.com/goplus/gox v1.9.7
+	github.com/json-iterator/go v1.1.12
+)
