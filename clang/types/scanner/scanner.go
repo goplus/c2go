5ca87881f7bfb16608ff8a27567a7182e808bbf0
xushiwei <x@goplus.org>
2022-03-27 04:06:55 +0800

    types.scanner: remove unused

--- clang/types/scanner/scanner.go
+++ clang/types/scanner/scanner.go
@@ -235,46 +235,23 @@ exit:
 	return string(s.src[offs:s.offset])
 }
 
-func digitVal(ch rune) int {
-	switch {
-	case '0' <= ch && ch <= '9':
-		return int(ch - '0')
-	case 'a' <= lower(ch) && lower(ch) <= 'f':
-		return int(lower(ch) - 'a' + 10)
-	}
-	return 16 // larger than any legal digit val
-}
-
 func lower(ch rune) rune     { return ('a' - 'A') | ch } // returns lower-case ch iff ch is ASCII letter
 func isDecimal(ch rune) bool { return '0' <= ch && ch <= '9' }
 func isHex(ch rune) bool     { return '0' <= ch && ch <= '9' || 'a' <= lower(ch) && lower(ch) <= 'f' }
 
-// digits accepts the sequence { digit | '_' }.
-// If base <= 10, digits accepts any decimal digit but records
-// the offset (relative to the source start) of a digit >= base
-// in *invalid, if *invalid < 0.
-// digits returns a bitset describing whether the sequence contained
-// digits (bit 0 is set), or separators '_' (bit 1 is set).
 func (s *Scanner) digits(base int, invalid *int) (digsep int) {
 	if base <= 10 {
 		max := rune('0' + base)
-		for isDecimal(s.ch) || s.ch == '_' {
-			ds := 1
-			if s.ch == '_' {
-				ds = 2
-			} else if s.ch >= max && *invalid < 0 {
+		for isDecimal(s.ch) {
+			if s.ch >= max && *invalid < 0 {
 				*invalid = s.offset // record invalid rune offset
 			}
-			digsep |= ds
+			digsep = 1
 			s.next()
 		}
 	} else {
-		for isHex(s.ch) || s.ch == '_' {
-			ds := 1
-			if s.ch == '_' {
-				ds = 2
-			}
-			digsep |= ds
+		for isHex(s.ch) {
+			digsep = 1
 			s.next()
 		}
 	}
@@ -283,89 +260,39 @@ func (s *Scanner) digits(base int, invalid *int) (digsep int) {
 
 func (s *Scanner) scanNumber() (token.Token, string) {
 	offs := s.offset
-	tok := token.ILLEGAL
 
 	base := 10        // number base
 	prefix := rune(0) // one of 0 (decimal), '0' (0-octal), 'x', 'o', or 'b'
 	digsep := 0       // bit 0: digit present, bit 1: '_' present
 	invalid := -1     // index of invalid digit in literal, or < 0
 
-	// integer part
-	if s.ch != '.' {
-		tok = token.INT
-		if s.ch == '0' {
-			s.next()
-			switch lower(s.ch) {
-			case 'x':
-				s.next()
-				base, prefix = 16, 'x'
-			case 'o':
-				s.next()
-				base, prefix = 8, 'o'
-			case 'b':
-				s.next()
-				base, prefix = 2, 'b'
-			default:
-				base, prefix = 8, '0'
-				digsep = 1 // leading 0
-			}
-		}
-		digsep |= s.digits(base, &invalid)
-	}
-
-	// fractional part
-	if s.ch == '.' {
-		tok = token.FLOAT
-		if prefix == 'o' || prefix == 'b' {
-			s.error(s.offset, "invalid radix point in "+litname(prefix))
-		}
+	if s.ch == '0' {
 		s.next()
-		digsep |= s.digits(base, &invalid)
+		switch lower(s.ch) {
+		case 'x':
+			s.next()
+			base, prefix = 16, 'x'
+		case 'o':
+			s.next()
+			base, prefix = 8, 'o'
+		case 'b':
+			s.next()
+			base, prefix = 2, 'b'
+		default:
+			base, prefix = 8, '0'
+			digsep = 1 // leading 0
+		}
 	}
-
+	digsep |= s.digits(base, &invalid)
 	if digsep&1 == 0 {
 		s.error(s.offset, litname(prefix)+" has no digits")
 	}
 
-	// exponent
-	if e := lower(s.ch); e == 'e' || e == 'p' {
-		switch {
-		case e == 'e' && prefix != 0 && prefix != '0':
-			s.errorf(s.offset, "%q exponent requires decimal mantissa", s.ch)
-		case e == 'p' && prefix != 'x':
-			s.errorf(s.offset, "%q exponent requires hexadecimal mantissa", s.ch)
-		}
-		s.next()
-		tok = token.FLOAT
-		if s.ch == '+' || s.ch == '-' {
-			s.next()
-		}
-		ds := s.digits(10, nil)
-		digsep |= ds
-		if ds&1 == 0 {
-			s.error(s.offset, "exponent has no digits")
-		}
-	} else if prefix == 'x' && tok == token.FLOAT {
-		s.error(s.offset, "hexadecimal mantissa requires a 'p' exponent")
-	}
-
-	// suffix 'i'
-	if s.ch == 'i' {
-		tok = token.IMAG
-		s.next()
-	}
-
 	lit := string(s.src[offs:s.offset])
-	if tok == token.INT && invalid >= 0 {
+	if invalid >= 0 {
 		s.errorf(invalid, "invalid digit %q in %s", lit[invalid-offs], litname(prefix))
 	}
-	if digsep&2 != 0 {
-		if i := invalidSep(lit); i >= 0 {
-			s.error(offs+i, "'_' must separate successive digits")
-		}
-	}
-
-	return tok, lit
+	return token.INT, lit
 }
 
 func litname(prefix rune) string {
@@ -380,224 +307,10 @@ func litname(prefix rune) string {
 	return "decimal literal"
 }
 
-// invalidSep returns the index of the first invalid separator in x, or -1.
-func invalidSep(x string) int {
-	x1 := ' ' // prefix char, we only care if it's 'x'
-	d := '.'  // digit, one of '_', '0' (a digit), or '.' (anything else)
-	i := 0
-
-	// a prefix counts as a digit
-	if len(x) >= 2 && x[0] == '0' {
-		x1 = lower(rune(x[1]))
-		if x1 == 'x' || x1 == 'o' || x1 == 'b' {
-			d = '0'
-			i = 2
-		}
-	}
-
-	// mantissa and exponent
-	for ; i < len(x); i++ {
-		p := d // previous digit
-		d = rune(x[i])
-		switch {
-		case d == '_':
-			if p != '0' {
-				return i
-			}
-		case isDecimal(d) || x1 == 'x' && isHex(d):
-			d = '0'
-		default:
-			if p == '_' {
-				return i - 1
-			}
-			d = '.'
-		}
-	}
-	if d == '_' {
-		return len(x) - 1
-	}
-
-	return -1
-}
-
-// scanEscape parses an escape sequence where rune is the accepted
-// escaped quote. In case of a syntax error, it stops at the offending
-// character (without consuming it) and returns false. Otherwise
-// it returns true.
-func (s *Scanner) scanEscape(quote rune) bool {
-	offs := s.offset
-
-	var n int
-	var base, max uint32
-	switch s.ch {
-	case 'a', 'b', 'f', 'n', 'r', 't', 'v', '\\', quote:
-		s.next()
-		return true
-	case '0', '1', '2', '3', '4', '5', '6', '7':
-		n, base, max = 3, 8, 255
-	case 'x':
-		s.next()
-		n, base, max = 2, 16, 255
-	case 'u':
-		s.next()
-		n, base, max = 4, 16, unicode.MaxRune
-	case 'U':
-		s.next()
-		n, base, max = 8, 16, unicode.MaxRune
-	default:
-		msg := "unknown escape sequence"
-		if s.ch < 0 {
-			msg = "escape sequence not terminated"
-		}
-		s.error(offs, msg)
-		return false
-	}
-
-	var x uint32
-	for n > 0 {
-		d := uint32(digitVal(s.ch))
-		if d >= base {
-			msg := fmt.Sprintf("illegal character %#U in escape sequence", s.ch)
-			if s.ch < 0 {
-				msg = "escape sequence not terminated"
-			}
-			s.error(s.offset, msg)
-			return false
-		}
-		x = x*base + d
-		s.next()
-		n--
-	}
-
-	if x > max || 0xD800 <= x && x < 0xE000 {
-		s.error(offs, "escape sequence is invalid Unicode code point")
-		return false
-	}
-
-	return true
-}
-
-func (s *Scanner) scanRune() string {
-	// '\'' opening already consumed
-	offs := s.offset - 1
-
-	valid := true
-	n := 0
-	for {
-		ch := s.ch
-		if ch == '\n' || ch < 0 {
-			// only report error if we don't have one already
-			if valid {
-				s.error(offs, "rune literal not terminated")
-				valid = false
-			}
-			break
-		}
-		s.next()
-		if ch == '\'' {
-			break
-		}
-		n++
-		if ch == '\\' {
-			if !s.scanEscape('\'') {
-				valid = false
-			}
-			// continue to read to closing quote
-		}
-	}
-
-	if valid && n != 1 {
-		s.error(offs, "illegal rune literal")
-	}
-
-	return string(s.src[offs:s.offset])
-}
-
-func (s *Scanner) scanString() string {
-	// '"' opening already consumed
-	offs := s.offset - 1
-
-	for {
-		ch := s.ch
-		if ch == '\n' || ch < 0 {
-			s.error(offs, "string literal not terminated")
-			break
-		}
-		s.next()
-		if ch == '"' {
-			break
-		}
-		if ch == '\\' {
-			s.scanEscape('"')
-		}
-	}
-
-	return string(s.src[offs:s.offset])
-}
-
-func stripCR(b []byte, comment bool) []byte {
-	c := make([]byte, len(b))
-	i := 0
-	for j, ch := range b {
-		// In a /*-style comment, don't strip \r from *\r/ (incl.
-		// sequences of \r from *\r\r...\r/) since the resulting
-		// */ would terminate the comment too early unless the \r
-		// is immediately following the opening /* in which case
-		// it's ok because /*/ is not closed yet (issue #11151).
-		if ch != '\r' || comment && i > len("/*") && c[i-1] == '*' && j+1 < len(b) && b[j+1] == '/' {
-			c[i] = ch
-			i++
-		}
-	}
-	return c[:i]
-}
-
 func (s *Scanner) skipWhitespace() {
 	for s.ch == ' ' || s.ch == '\t' || s.ch == '\n' || s.ch == '\r' {
 		s.next()
 	}
-}
-
-// Helper functions for scanning multi-byte tokens such as >> += >>= .
-// Different routines recognize different length tok_i based on matches
-// of ch_i. If a token ends in '=', the result is tok1 or tok3
-// respectively. Otherwise, the result is tok0 if there was no other
-// matching character, or tok2 if the matching character was ch2.
-
-func (s *Scanner) switch2(tok0, tok1 token.Token) token.Token {
-	if s.ch == '=' {
-		s.next()
-		return tok1
-	}
-	return tok0
-}
-
-func (s *Scanner) switch3(tok0, tok1 token.Token, ch2 rune, tok2 token.Token) token.Token {
-	if s.ch == '=' {
-		s.next()
-		return tok1
-	}
-	if s.ch == ch2 {
-		s.next()
-		return tok2
-	}
-	return tok0
-}
-
-func (s *Scanner) switch4(tok0, tok1 token.Token, ch2 rune, tok2, tok3 token.Token) token.Token {
-	if s.ch == '=' {
-		s.next()
-		return tok1
-	}
-	if s.ch == ch2 {
-		s.next()
-		if s.ch == '=' {
-			s.next()
-			return tok3
-		}
-		return tok2
-	}
-	return tok0
 }
 
 func (s *Scanner) Scan() (pos token.Pos, tok token.Token, lit string) {
@@ -611,7 +324,7 @@ func (s *Scanner) Scan() (pos token.Pos, tok token.Token, lit string) {
 	case isLetter(ch):
 		lit = s.scanIdentifier()
 		tok = token.IDENT
-	case isDecimal(ch) || ch == '.' && isDecimal(rune(s.peek())):
+	case isDecimal(ch):
 		tok, lit = s.scanNumber()
 	default:
 		s.next() // always make progress
