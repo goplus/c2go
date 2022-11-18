cca496580d5b0485ec5d7fa21a52cfce632b0d99
visualfc <visualfc@gmail.com>
2022-11-18 20:32:09 +0800

    testdata/strlen

--- testdata/strlen/libc_windows.go
+++ testdata/strlen/libc_windows.go
@@ -1,6 +1,3 @@
-//go:build !windows
-// +build !windows
-
 package main
 
 import (
@@ -35,4 +32,16 @@ func printf(format *int8, args ...interface{}) int32 {
 	return 0
 }
 
-type struct___locale_data struct{}
+type struct___lc_time_data struct {
+}
+type struct_lconv struct {
+}
+type struct_threadmbcinfostruct struct {
+}
+
+func strnlen(_Str *int8, _MaxCount uint64) uint64 {
+	panic("notimpl")
+}
+func wcsnlen(_Src *uint16, _MaxCount uint64) uint64 {
+	panic("notimpl")
+}
